1d754037147724c74bf2be3219adcb483517a692
Daniele <daniele.taverriti@gmail.com>
2026-03-09 18:27:54 +0100

    start pulito

--- lib/main.dart
+++ lib/main.dart
@@ -4,19 +4,6 @@ import 'services/boot.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  try {
-    await Boot.init();
-    runApp(const QuantoPossoApp());
-  } catch (e) {
-    runApp(
-      MaterialApp(
-        home: Scaffold(
-          body: Center(
-            child: Text('BOOT ERROR: $e'),
-          ),
-        ),
-      ),
-    );
-  }
+  await Boot.init();
+  runApp(const QuantoPossoApp());
 }
